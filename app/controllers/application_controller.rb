3768bbca4b0bbdba38a49e682740b84397a9d6d7
jlarivaux <jplarivaux@gmail.com>
2024-01-26 19:36:46 -0800

    search bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
     before_action :set_query 
 
     def set_query
-        
+        @query = Post.ransack(params[:q])
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,14 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
 
+
+
+  #chat code below
   def self.ransackable_attributes(auth_object = nil)
     ["body", "created_at", "id", "title", "updated_at", "user_id", "views"]
   end
-  
+
+  def self.ransackable_associations(auth_object = nil)
+    ["comments", "user"]
+  end
 end
